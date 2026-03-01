8e6856fe044a1299ae967f8f3f306a45ca320f03
littleca <carrieannelittle61@gmail.com>
2026-03-01 11:57:56 -0600

    comments for GapBuffer

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -6,13 +6,20 @@ package edu.grinnell.csc207.texteditor;
 public class GapBuffer implements Buffer {
     private int size, cursor, endGap;
     private char[] buff;
+    /**
+     * Constructs an empty GapBuffer object of size 10.
+     */
     public GapBuffer() {
         size = 10;
         cursor = 0;
         endGap = size;
         buff = new char[size];
     }
-
+    
+    /**
+     * Doubles the size of the buffer, 
+     * adding the new space between cursor and endgap.
+     */
     public void expandBuffer() {
         char[] old = buff;
         buff = new char[size * 2];
@@ -24,9 +31,13 @@ public class GapBuffer implements Buffer {
         }
         endGap += size;
         size *= 2;
-        
     }
 
+    /**
+     * Checks if buffer needs to be expanded and does so if so.
+     * Adds ch to buffer and shifts cursor right.
+     * @param ch character: to be added to buffer
+     */
     public void insert(char ch) {
         if (cursor>=endGap) {
             this.expandBuffer();
@@ -35,32 +46,53 @@ public class GapBuffer implements Buffer {
         cursor++;
     }
 
+    /**
+     * Moves cursor to the left. endGap does not move.
+     */
     public void delete() {
         if(cursor != 0) {
             cursor--;
         }
     }
 
+    /**
+     * @return integer: index of cursor
+     */
     public int getCursorPosition() {
         return cursor;
     }
 
+    /**
+     * If cursor is not already at front of buffer, 
+     * shifts entire gap left one character in the buffer
+     */
     public void moveLeft() {
         if (cursor>0) {
             buff[--endGap] = buff[--cursor];
         }
     }
-
+    
+    /**
+     * If endGap is not already at end of buffer,
+     * shifts entire gap right one character in buffer
+     */
     public void moveRight() {
         if (endGap<size) {
             buff[cursor++] = buff[endGap++];
         }
     }
 
+    /**
+     * @return int: number of chars in buffer (excludes gap)
+     */
     public int getSize() {
         return size - (endGap - cursor);
     }
 
+    /**
+     * @param i integer: index in buffer
+     * @return character at specified index (excludes gap)
+     */
     public char getChar(int i) {
         if(i<cursor) {
             return buff[i];
@@ -69,6 +101,9 @@ public class GapBuffer implements Buffer {
         }
     }
 
+    /**
+     * @return String of all characters in buffer (excludes gap)
+     */
     public String toString() {
         String output = "";
         for(int i = 0; i<cursor; i++) {
